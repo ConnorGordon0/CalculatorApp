23e5b486ac0c6ce90f2507866600c8fa5c3c3a7b
Connor Gordon <cgordon@ghostrider.highpoint.edu>
2017-02-27 21:44:57 -0500

    First Commit and Push. Added Aarons Name and made mirror changes

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -2,20 +2,23 @@
 //  ViewController.swift
 //  CalculatorApp
 //
-//  Created by Connor Gordon on 2/27/17.
+//  Created by Connor Gordon and Aaron Marlowe 2/27/17.
 //  Copyright © 2017 Connor Gordon. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
